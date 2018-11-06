d957cc792af5056ee4a7e1e4a10c1df1946ef6ef
Cecilia Assis <cy_assis@hotmail.com>
2018-11-06 00:59:31 -0200

    Add solver file

---
+++ lib/simplex_solver/operations/solve_tableau.rb
@@ -0,0 +1,14 @@
+require 'matrix'
+
+module SimplexSolver
+  module Operations
+    # BuildTableau
+    class SolveTableau
+        def initialize(tableau)
+        end
+
+        def self.solve(tableau)
+        end
+    end
+  end
+end
